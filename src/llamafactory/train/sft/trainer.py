03b0af9e05498b2c80a852cd65929614a3080c75
JJVV <junjievv@qq.com>
2025-01-06 16:52:41 +0800

    custom trainer to output scores

--- src/llamafactory/train/sft/trainer.py
+++ src/llamafactory/train/sft/trainer.py
@@ -129,20 +129,25 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
             if label_len > prompt_len:  # truncate the labels instead of padding the inputs (llama2 fp16 compatibility)
                 inputs["labels"] = inputs["labels"][:, :prompt_len]
 
-        loss, generated_tokens, _ = super().prediction_step(  # ignore the returned labels (may be truncated)
-        # loss, generated_tokens, _ = self.prediction_step_with_score(  # ignore the returned labels (may be truncated)
-            model, inputs, prediction_loss_only=prediction_loss_only, ignore_keys=ignore_keys
+        # loss, generated_tokens, _ = super().prediction_step(  # ignore the returned labels (may be truncated)
+        loss, generated_tokens, _ = self.prediction_step_with_score_custom(  # ignore the returned labels (may be truncated)
+            model, inputs, 
+            prediction_loss_only=prediction_loss_only, ignore_keys=ignore_keys,
         )
         if generated_tokens is not None and self.args.predict_with_generate:
             generated_tokens[:, :prompt_len] = self.processing_class.pad_token_id
             generated_tokens = generated_tokens.contiguous()
-
+        
+        print(loss)
+        print(generated_tokens)
+        print(labels)
+        exit()
         return loss, generated_tokens, labels
 
     # =====
-    def prediction_step_with_score(
+    def prediction_step_with_score_custom(
         self,
-        model: "torch.nn.Module",
+        model: 'torch.nn.Module',
         inputs: Dict[str, Union[torch.Tensor, Any]],
         prediction_loss_only: bool,
         ignore_keys: Optional[List[str]] = None,
@@ -179,67 +184,108 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
         has_labels = "labels" in inputs
         inputs = self._prepare_inputs(inputs)
 
-        # XXX: adapt synced_gpus for fairscale as well
         # Priority (handled in generate):
-        # gen_kwargs > model.generation_config > default GenerationConfig()
-
+        # non-`None` gen_kwargs > model.generation_config > default GenerationConfig()
         if len(gen_kwargs) == 0 and hasattr(self, "_gen_kwargs"):
             gen_kwargs = self._gen_kwargs.copy()
+        if "num_beams" in gen_kwargs and gen_kwargs["num_beams"] is None:
+            gen_kwargs.pop("num_beams")
+        if "max_length" in gen_kwargs and gen_kwargs["max_length"] is None:
+            gen_kwargs.pop("max_length")
 
-        if gen_kwargs.get("max_length") is None and gen_kwargs.get("max_new_tokens") is None:
-            gen_kwargs["max_length"] = self.model.config.max_length
-        gen_kwargs["num_beams"] = (
-            gen_kwargs["num_beams"] if gen_kwargs.get("num_beams") is not None else self.model.config.num_beams
-        )
-        default_synced_gpus = True if is_deepspeed_zero3_enabled() else False
-        gen_kwargs["synced_gpus"] = (
-            gen_kwargs["synced_gpus"] if gen_kwargs.get("synced_gpus") is not None else default_synced_gpus
-        )
+        from transformers.integrations.deepspeed import is_deepspeed_zero3_enabled
+        from transformers.integrations.fsdp import is_fsdp_managed_module
+        default_synced_gpus = is_deepspeed_zero3_enabled() or is_fsdp_managed_module(self.model)
+        gen_kwargs["synced_gpus"] = gen_kwargs.get("synced_gpus", default_synced_gpus)
 
+        generation_inputs = inputs.copy()
         # If the `decoder_input_ids` was created from `labels`, evict the former, so that the model can freely generate
         # (otherwise, it would continue generating from the padded `decoder_input_ids`)
         if (
-            "labels" in inputs
-            and "decoder_input_ids" in inputs
-            and inputs["labels"].shape == inputs["decoder_input_ids"].shape
+            "labels" in generation_inputs
+            and "decoder_input_ids" in generation_inputs
+            and generation_inputs["labels"].shape == generation_inputs["decoder_input_ids"].shape
         ):
-            inputs = {k: v for k, v in inputs.items() if k != "decoder_input_ids"}
+            generation_inputs = {
+                k: v for k, v in inputs.items() if k not in ("decoder_input_ids", "decoder_attention_mask")
+            }
+
+        import contextlib
+        from torch.distributed.fsdp import FullyShardedDataParallel
+        summon_full_params_context = (
+            FullyShardedDataParallel.summon_full_params(self.model)
+            if isinstance(self.model, FullyShardedDataParallel)
+            else contextlib.nullcontext()
+        )
 
         # =====
-        # from utils_zp import *
-        # import utils_zp
-        from utils_zp import auto_load, auto_dump
+        # -- old ---
+        # with summon_full_params_context:
+        #     generated_tokens = self.model.generate(**inputs, **custome_gen_kwargs)
+        # -- old ---
+
+        from utils_zp import path, auto_load, auto_dump, tensor_to_list, dcopy
+
+        custome_gen_kwargs = dcopy(gen_kwargs)
+        custome_gen_kwargs['return_dict_in_generate'] = True
+        custome_gen_kwargs['output_scores'] = True
+        with summon_full_params_context:
+            generated_tokens = self.model.generate(**inputs, **custome_gen_kwargs)
+        sequences, scores, past_key_values = (
+            generated_tokens['sequences'],
+            generated_tokens['scores'],
+            generated_tokens['past_key_values'],
+        )
+
         src_output_dir = path(self.args.output_dir)
         root_output_dir = src_output_dir.parent
         extra_setting = auto_load(root_output_dir / 'main_config.json')['extra_setting']
         if extra_setting['output_scores']:
-            score_output_path = src_output_dir / 'generated_scores.jsonl'
-            
-            gen_kwargs['return_dict_in_generate'] = True
-            gen_kwargs['output_scores'] = True
-            generated_tokens = self.model.generate(**inputs, **gen_kwargs)
-            seq = generated_tokens['sequences']
-            seq = seq[0]
-            scores = []
+            # print(self.tokenizer.batch_decode(sequences[:, :prompt_len]))
+            # print(self.tokenizer.batch_decode(sequences[:, prompt_len:]))
+            # seq_len = sequences.shape[1]
+            # print(seq_len, prompt_len, seq_len-prompt_len)
+            # print(len(scores))
             prompt_len = inputs["input_ids"].size(-1)
-            for pid, score in enumerate(generated_tokens['scores']):
-                scores.append(score[0,seq[pid+prompt_len]])
-            try:
-                scores = torch.tensor(scores).detach().cpu().tolist()
-            except:
-                print(scores)
-                exit()
+            output_str = self.tokenizer.batch_decode(
+                sequences[:, prompt_len:], 
+                skip_special_tokens=True,
+            )
+            output_seq = sequences[0, prompt_len:]
+            output_scores = [
+                scores[pid][0, output_seq[pid]]
+                for pid in range(output_seq.shape[0])
+            ]
+            label_str = self.tokenizer.batch_decode(inputs['labels'], skip_special_tokens=True)
+
+            auto_dump(
+                {
+                    'output_str': output_str,
+                    'output_seq': tensor_to_list(output_seq),
+                    'output_scores': output_scores,
+                    'label_str': label_str,
+                },
+                # path('/public/home/hongy/zpwang/LLaMA-Factory_zp/src/scripts/sga100', 'c.json')
+                src_output_dir / 'generated_scores.jsonl',
+            )
+            # _scores = []
+            # prompt_len = inputs["input_ids"].size(-1)
+            # for pid, score in enumerate(scores):
+            #     scores.append(score[0,seq[pid+prompt_len]])
+            # try:
+            #     scores = torch.tensor(scores).detach().cpu().tolist()
+            # except:
+            #     print(scores)
+            #     exit()
             
-            real_label = self.tokenizer.batch_decode(real_labels)[0]
-            real_label = real_label.replace('<|eot_id|>', '')
-            score_info = {'label': real_label, 'scores': scores}
-            auto_dump(score_info, score_output_path)
-            
-            gen_kwargs['return_dict_in_generate'] = False
-            gen_kwargs['output_scores'] = False
-        # =====
+            # real_label = self.tokenizer.batch_decode(real_labels)[0]
+            # real_label = real_label.replace('<|eot_id|>', '')
+            # score_info = {'label': real_label, 'scores': scores}
+            # auto_dump(score_info, score_output_path)
         
-        generated_tokens = self.model.generate(**inputs, **gen_kwargs)
+        generated_tokens = generated_tokens['sequences']
+        # exit()
+        # =====
 
         # Temporary hack to ensure the generation config is not initialized for each iteration of the evaluation loop
         # TODO: remove this hack when the legacy code that initializes generation_config from a model config is
@@ -279,6 +325,146 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
             labels = None
 
         return loss, generated_tokens, labels
+
+    # def prediction_step_with_score(
+    #     self,
+    #     model: "torch.nn.Module",
+    #     inputs: Dict[str, Union[torch.Tensor, Any]],
+    #     prediction_loss_only: bool,
+    #     ignore_keys: Optional[List[str]] = None,
+    #     **gen_kwargs,
+    # ) -> Tuple[Optional[float], Optional[torch.Tensor], Optional[torch.Tensor]]:
+    #     """
+    #     Perform an evaluation step on `model` using `inputs`.
+
+    #     Subclass and override to inject custom behavior.
+
+    #     Args:
+    #         model (`nn.Module`):
+    #             The model to evaluate.
+    #         inputs (`Dict[str, Union[torch.Tensor, Any]]`):
+    #             The inputs and targets of the model.
+
+    #             The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+    #             argument `labels`. Check your model's documentation for all accepted arguments.
+    #         prediction_loss_only (`bool`):
+    #             Whether or not to return the loss only.
+    #         gen_kwargs:
+    #             Additional `generate` specific kwargs.
+
+    #     Return:
+    #         Tuple[Optional[float], Optional[torch.Tensor], Optional[torch.Tensor]]: A tuple with the loss, logits and
+    #         labels (each being optional).
+    #     """
+
+    #     if not self.args.predict_with_generate or prediction_loss_only:
+    #         return super().prediction_step(
+    #             model, inputs, prediction_loss_only=prediction_loss_only, ignore_keys=ignore_keys
+    #         )
+
+    #     has_labels = "labels" in inputs
+    #     inputs = self._prepare_inputs(inputs)
+
+    #     # XXX: adapt synced_gpus for fairscale as well
+    #     # Priority (handled in generate):
+    #     # gen_kwargs > model.generation_config > default GenerationConfig()
+
+    #     if len(gen_kwargs) == 0 and hasattr(self, "_gen_kwargs"):
+    #         gen_kwargs = self._gen_kwargs.copy()
+
+    #     if gen_kwargs.get("max_length") is None and gen_kwargs.get("max_new_tokens") is None:
+    #         gen_kwargs["max_length"] = self.model.config.max_length
+    #     gen_kwargs["num_beams"] = (
+    #         gen_kwargs["num_beams"] if gen_kwargs.get("num_beams") is not None else self.model.config.num_beams
+    #     )
+    #     default_synced_gpus = True if is_deepspeed_zero3_enabled() else False
+    #     gen_kwargs["synced_gpus"] = (
+    #         gen_kwargs["synced_gpus"] if gen_kwargs.get("synced_gpus") is not None else default_synced_gpus
+    #     )
+
+    #     # If the `decoder_input_ids` was created from `labels`, evict the former, so that the model can freely generate
+    #     # (otherwise, it would continue generating from the padded `decoder_input_ids`)
+    #     if (
+    #         "labels" in inputs
+    #         and "decoder_input_ids" in inputs
+    #         and inputs["labels"].shape == inputs["decoder_input_ids"].shape
+    #     ):
+    #         inputs = {k: v for k, v in inputs.items() if k != "decoder_input_ids"}
+
+    #     # =====
+    #     # from utils_zp import *
+    #     # import utils_zp
+    #     from utils_zp import auto_load, auto_dump, path
+    #     src_output_dir = path(self.args.output_dir)
+    #     root_output_dir = src_output_dir.parent
+    #     extra_setting = auto_load(root_output_dir / 'main_config.json')['extra_setting']
+    #     if extra_setting['output_scores']:
+    #         score_output_path = src_output_dir / 'generated_scores.jsonl'
+            
+    #         gen_kwargs['return_dict_in_generate'] = True
+    #         gen_kwargs['output_scores'] = True
+    #         generated_tokens = self.model.generate(**inputs, **gen_kwargs)
+    #         seq = generated_tokens['sequences']
+    #         seq = seq[0]
+    #         scores = []
+    #         prompt_len = inputs["input_ids"].size(-1)
+    #         for pid, score in enumerate(generated_tokens['scores']):
+    #             scores.append(score[0,seq[pid+prompt_len]])
+    #         try:
+    #             scores = torch.tensor(scores).detach().cpu().tolist()
+    #         except:
+    #             print(scores)
+    #             exit()
+            
+    #         real_label = self.tokenizer.batch_decode(real_labels)[0]
+    #         real_label = real_label.replace('<|eot_id|>', '')
+    #         score_info = {'label': real_label, 'scores': scores}
+    #         auto_dump(score_info, score_output_path)
+            
+    #         gen_kwargs['return_dict_in_generate'] = False
+    #         gen_kwargs['output_scores'] = False
+    #     # =====
+        
+    #     generated_tokens = self.model.generate(**inputs, **gen_kwargs)
+
+    #     # Temporary hack to ensure the generation config is not initialized for each iteration of the evaluation loop
+    #     # TODO: remove this hack when the legacy code that initializes generation_config from a model config is
+    #     # removed in https://github.com/huggingface/transformers/blob/98d88b23f54e5a23e741833f1e973fdf600cc2c5/src/transformers/generation/utils.py#L1183
+    #     if self.model.generation_config._from_model_config:
+    #         self.model.generation_config._from_model_config = False
+
+    #     # Retrieves GenerationConfig from model.generation_config
+    #     gen_config = self.model.generation_config
+    #     # in case the batch is shorter than max length, the output should be padded
+    #     if generated_tokens.shape[-1] < gen_config.max_length:
+    #         generated_tokens = self._pad_tensors_to_max_len(generated_tokens, gen_config.max_length)
+    #     elif gen_config.max_new_tokens is not None and generated_tokens.shape[-1] < gen_config.max_new_tokens + 1:
+    #         generated_tokens = self._pad_tensors_to_max_len(generated_tokens, gen_config.max_new_tokens + 1)
+
+    #     with torch.no_grad():
+    #         if has_labels:
+    #             with self.compute_loss_context_manager():
+    #                 outputs = model(**inputs)
+    #             if self.label_smoother is not None:
+    #                 loss = self.label_smoother(outputs, inputs["labels"]).mean().detach()
+    #             else:
+    #                 loss = (outputs["loss"] if isinstance(outputs, dict) else outputs[0]).mean().detach()
+    #         else:
+    #             loss = None
+
+    #     if self.args.prediction_loss_only:
+    #         return loss, None, None
+
+    #     if has_labels:
+    #         labels = inputs["labels"]
+    #         if labels.shape[-1] < gen_config.max_length:
+    #             labels = self._pad_tensors_to_max_len(labels, gen_config.max_length)
+    #         elif gen_config.max_new_tokens is not None and labels.shape[-1] < gen_config.max_new_tokens + 1:
+    #             labels = self._pad_tensors_to_max_len(labels, gen_config.max_new_tokens + 1)
+    #     else:
+    #         labels = None
+
+    #     return loss, generated_tokens, labels
 
     def _pad_tensors_to_target_len(self, src_tensor: "torch.Tensor", tgt_tensor: "torch.Tensor") -> "torch.Tensor":
         r"""
